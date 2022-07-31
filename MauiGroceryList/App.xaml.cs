af10f7ba42d6007b09f567face6fdc763f2c728f
Andrew O'Brien <abo@abobrien.com>
2022-07-31 19:36:25 -0400

    Add project files.

---
+++ MauiGroceryList/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiGroceryList;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiGroceryList/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiGroceryList;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
