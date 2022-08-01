5fed4effefee2352fe36b438440ab60e83469480
Andrew O'Brien <abo@abobrien.com>
2022-07-31 21:12:33 -0400

    Initial commit

--- MauiGroceryList/MauiProgram.cs
+++ MauiGroceryList/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiGroceryList;
+﻿using MauiGroceryList.ViewModel;
+
+namespace MauiGroceryList;
 
 public static class MauiProgram
 {
@@ -12,6 +14,9 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+
+		builder.Services.AddSingleton<MainPage>();
+		builder.Services.AddSingleton<MainViewModel>();
 
 		return builder.Build();
 	}
---
+++ MauiGroceryList/ViewModel/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace MauiGroceryList.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    public MainViewModel()
+    {
+        Items = new ObservableCollection<string>();
+    }
+
+    [ObservableProperty]
+    ObservableCollection<string> items;
+
+    [ObservableProperty]
+    string text;
+
+    [RelayCommand]
+    void Add()
+    {
+        if (string.IsNullOrWhiteSpace(Text))
+            return;
+
+        // add item to list
+        Items.Add(Text);
+
+        // clear text box
+        Text = string.Empty;
+    }
+
+    [RelayCommand]
+    void Delete(string s)
+    {
+        if (items.Contains(s))
+        {
+            Items.Remove(s);
+        }
+    }
+}
